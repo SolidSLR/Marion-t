9f24b34c2f857d3ec577222f3adffd3208319406
Santiago <devsantilopez@gmail.com>
2021-12-10 19:35:32 +0100

    Añadidas animaciones de frenada, controlado el inicio y el final del movimiento y alguna cosa más

--- Assets/Scripts/Mario.cs
+++ Assets/Scripts/Mario.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Mario : MonoBehaviour
 {
-    private float speed = 3f; 
+    //Velocidad de movimiento absoluta de Mariont
+    private float speed = 3f;
+    //Velocididad de movimiento actual (para controlar deceleración) 
+    private float actualSpeed;
+    //Deceleración
+    private float deceleration = 7f;
+    //Controlador del animador
     Animator animator;
+    //Controlador del Rigidbody
     private Rigidbody2D rb;
-
+    //Controlador de la velocidad del salto
     private float jump = 6.5f;
+    //Variable para impedir que se acelere hacia ningún lado mientras estás frenando
+    private bool stopping = false;
     
     // Start is called before the first frame update
     void Start()
@@ -20,23 +29,58 @@ public class Mario : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        animator.SetBool("Walking", false);
-        if(Input.GetKey(KeyCode.LeftArrow)){
-            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.Self);
+        //animator.SetBool("Walking", false);
+
+        animator.SetBool("Jumping", false);
+        
+        //actualSpeed = speed;
+
+        //Comprobamos que Mariont no esté parado, la tecla pulsada y evitamos que se mueva al lado 
+        //contrario si no se ha parado antes de cambiar de sentido
+        if(!stopping && Input.GetKey(KeyCode.LeftArrow) && actualSpeed<=0){
+            //transform.Translate(Vector3.left * actualSpeed * Time.deltaTime, Space.Self);
+            //Si Mariont se mueve a la izquierda, asignamos a actualSpeed la velocidad negativa
+            actualSpeed = -speed;
             animator.SetBool("Walking", true);
             transform.localScale = new Vector3(1,1,1);
-        }else if(Input.GetKey(KeyCode.RightArrow)){
-            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.Self);
+
+        }else if(!stopping && Input.GetKey(KeyCode.RightArrow) && actualSpeed >=0){
+            //transform.Translate(Vector3.right * actualSpeed * Time.deltaTime, Space.Self);
+            //Asignamos a actualSpeed la velocidad positiva
+            actualSpeed = speed;
             animator.SetBool("Walking", true);
             transform.localScale = new Vector3(-1,1,1);
-        }else {
+
+        }else if(actualSpeed!=0){
+
+            stopping = true;
+            animator.SetBool("Stopping", true);
             animator.SetBool("Walking", false);
+
+            if(actualSpeed>0){
+
+                actualSpeed -= deceleration*Time.deltaTime;
+
+            }else if(actualSpeed<0){
+
+                actualSpeed += deceleration*Time.deltaTime;
+            }
+            Debug.Log(actualSpeed);
+            if(Mathf.Abs(actualSpeed)<0.01f){
+                stopping = false;
+                animator.SetBool("Stopping", false);
+                actualSpeed = 0;
+                //animator.SetBool("Stopping", false);
+            }
         }
+
+        transform.Translate(Vector3.right * actualSpeed * Time.deltaTime, Space.Self);
         
-        if (Input.GetKeyDown(KeyCode.UpArrow)){
+        /*if (Input.GetKeyDown(KeyCode.UpArrow)){
             rb.AddForce(Vector2.up*jump, ForceMode2D.Impulse);
-        }/*else {
-            animator.SetBool("Walking", false);
+            animator.SetBool("Jumping", true);
+        }else {
+            animator.SetBool("Jumping", false);
         }*/
     }
 }
